07ea29cd8156e3bc01d773c793bda3c3aca1dfc2
royhandf <royhandf@gmail.com>
2025-12-26 18:53:32 +0700

    fix: reduce toast notification duration

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
           {children}
           <Toaster
             position="top-center"
+            duration={3000}
             toastOptions={{
               classNames: {
                 toast: "bg-white border border-gray-200 shadow-lg",
